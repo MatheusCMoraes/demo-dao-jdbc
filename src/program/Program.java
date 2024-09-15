4e8e0471553baadfe3f4a40f61da686b6f90374c
Matheus Moraes <matheurmoraes635@gmail.com>
2024-09-15 15:57:15 -0300

    Reusing instantiation

--- src/program/Program.java
+++ src/program/Program.java
@@ -1,60 +1,21 @@
 package program;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import db.DB;
-import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		
-		Connection conn = null;
-			
-		Statement st = null;
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		try {
-			
-			
-			conn = DB.getConnection();
-			
-			conn.setAutoCommit(false);
-			
-			st = conn.createStatement();
-			
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-			
-			int x = 1;
-			if(x < 2) {
-				throw new SQLException("Fake error");
-				
-			}
-			
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-			
-			conn.commit();
-			
-			System.out.println("rows1: " + rows1);
-			
-			System.out.println("rows2: " + rows2);
+		System.out.println("=== TEST 1: seller findById =====");
+		Seller seller = sellerDao.findById(3);
 		
-		}catch(SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
-			} catch (SQLException e1) {
-				//erro no rollback
-				throw new DbException("Error trying to rollback! Caused by: " + e.getMessage());
-			}
-		}
-		finally {
-			
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		
+		System.out.println(seller);
 		
 	}
 
